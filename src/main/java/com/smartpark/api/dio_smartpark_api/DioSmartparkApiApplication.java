f5f4c0eba6db22c0d2f8fc80e3bd526ba325504a
Victor Lima <1613658+victorvlima@users.noreply.github.com>
2025-09-19 09:34:55 -0300

    Base

---
+++ src/main/java/com/smartpark/api/dio_smartpark_api/DioSmartparkApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartpark.api.dio_smartpark_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSmartparkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSmartparkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartpark/api/dio_smartpark_api/DioSmartparkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpark.api.dio_smartpark_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSmartparkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
