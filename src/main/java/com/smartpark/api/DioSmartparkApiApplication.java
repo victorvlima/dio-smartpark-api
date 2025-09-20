2d4aab48dfbdf504d4c72aa2b459c732926387ea
Victor Lima <1613658+victorvlima@users.noreply.github.com>
2025-09-19 23:11:01 -0300

    Correção em package

--- src/main/java/com/smartpark/api/DioSmartparkApiApplication.java
+++ src/main/java/com/smartpark/api/DioSmartparkApiApplication.java
@@ -1,4 +1,4 @@
-package com.smartpark.api.dio_smartpark_api;
+package com.smartpark.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
